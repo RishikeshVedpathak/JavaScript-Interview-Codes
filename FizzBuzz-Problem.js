b6029c350426ce801d68dbf387d28e84acbcf3ee
Rishikesh Vedpathak <rishikeshvedpathak@gmail.com>
2016-04-25 20:15:38 +0530

    FizzBuzz Problem

---
+++ FizzBuzz-Problem.js
@@ -0,0 +1,25 @@
+/*
+
+FizzBuzz problem
+If number divides evenly with 3 - returns string "fizz"
+If number divides evenly with 5 - returns string "buzz"
+If number divides evenly with 3 and 5 - returns string "fizz buzz"
+
+Example:
+fizzbuzz(1)==1
+fizzbuzz(9)=="fizz"
+fizzbuzz(25)=="buzz"
+fizzbuzz(37)==37
+fizzbuzz(45)=="fizz buzz"
+
+
+*/
+
+function fizzbuzz(num) {
+    var msg = "";
+    if (!(num % 3)) msg += "Fizz";
+    if (!(num % 5)) msg += "Buzz";
+    console.log(msg || i);
+}
+
+fizzbuzz(45);
